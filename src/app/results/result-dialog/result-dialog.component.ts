7e8afad38e4cdf294468cb84e822471f9d3566d2
RahulSainy <kradekidrahul@gmail.com>
2023-09-27 09:15:19 +0530

    registered teams view

--- src/app/results/result-dialog/result-dialog.component.ts
+++ src/app/results/result-dialog/result-dialog.component.ts
@@ -1,7 +1,9 @@
-import { Component,Inject } from '@angular/core';
+import { Component,Inject, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ResultsService } from '../results.service';
 import { map } from 'rxjs/operators';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-result-dialog',
@@ -9,29 +11,33 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./result-dialog.component.css']
 })
 export class ResultDialogComponent{
+  Round = 'All';
+  displayedColumns: string[] = ['groupNumber', 'teamName', 'teamLeader', 'college'];
+  dataSource: MatTableDataSource<any>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
   constructor(
     public dialogRef: MatDialogRef<ResultDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public theme: any,
     private resultService: ResultsService
-  ) {}
+  ) {
+    this.dataSource = new MatTableDataSource<any>();
+  }
 
-  Round = 'Round 1';
-  displayedColumns: string[] = ['groupNumber', 'teamName', 'teamLeader', 'college'];
-  dataSource: any[] = [];
-
-  ngOnInit() {
+  ngOnInit(): void {
     this.loadDataFromFirebase();
   }
 
   loadDataFromFirebase(): void {
-    this.resultService.getAll().snapshotChanges().subscribe((data: any) => {
-      // Assuming the data structure matches your Firebase data
-      this.dataSource = data.map((item: any, index: number) => ({
+    this.resultService.getAll().snapshotChanges().subscribe(data => {
+      const transformedData = data.map(item => ({
         groupNumber: item.key, // Use the group number as-is
         teamName: item.payload.val()['Team Leader College Name'],
         teamLeader: item.payload.val()['Team Leader Name'],
         college: item.payload.val()['Team Leader College Name'],
       }));
+      this.dataSource.data = transformedData;
+      this.dataSource.paginator = this.paginator; // Attach paginator to the data source
     });
   }
 
